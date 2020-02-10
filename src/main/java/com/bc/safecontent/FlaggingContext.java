db621de41763a05c8f8d83edf045be8242309ca8
poshjosh <posh.bc@gmail.com>
2020-02-10 10:14:31 +0100

    Make minor refactoring

--- src/main/java/com/bc/safecontent/FlaggingContext.java
+++ src/main/java/com/bc/safecontent/FlaggingContext.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.bc.safecontent;
 
 import com.bc.safecontent.util.Collector;
