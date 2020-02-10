dabbd5b6ecfdbfcc46a72b8ce594ef0f3d4f2d78
poshjosh <posh.bc@gmail.com>
2020-02-11 00:30:47 +0100

    Add @EnableDiscoveryClient to use spring cloud microservices infrastructure

--- src/main/java/com/bc/safecontent/SafeContentApplication.java
+++ src/main/java/com/bc/safecontent/SafeContentApplication.java
@@ -18,12 +18,14 @@ package com.bc.safecontent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author USER
  */
-@SpringBootApplication
 @EnableCaching
+@SpringBootApplication
+@EnableDiscoveryClient
 public class SafeContentApplication {
     public static void main(String[] args) {
         SpringApplication.run(SafeContentApplication.class, args);
